876ec8882278fa53e30d4a4cb2f23d960249bed4
galaslid <galaslid@roehampton.ac.uk>
2025-03-30 15:59:17 +0100

    Add CheckDefenderStatusAsync method to MicrosoftSecurityService

--- Working/Services/MicrosoftSecurityService.cs
+++ Working/Services/MicrosoftSecurityService.cs
@@ -116,4 +116,47 @@ public class MicrosoftSecurityService
             };
         }
     }
+
+    public async Task<DefenderStatus> CheckDefenderStatusAsync(string driverPath)
+    {
+        try
+        {
+            var status = new DefenderStatus
+            {
+                IsClean = true,
+                ThreatLevel = "Unknown",
+                LastScanTime = DateTime.Now
+            };
+
+            using var searcher = new ManagementObjectSearcher("root\\Microsoft\\Windows\\Defender", 
+                $"SELECT * FROM MSFT_MpThreat WHERE Path = '{driverPath}'");
+            using var collection = searcher.Get();
+
+            foreach (var threat in collection)
+            {
+                var threatLevel = threat.GetPropertyValue("ThreatLevel")?.ToString();
+                var lastScanTime = threat.GetPropertyValue("LastScanTime")?.ToString();
+                
+                status.IsClean = false;
+                status.ThreatLevel = threatLevel ?? "Unknown";
+                if (DateTime.TryParse(lastScanTime, out DateTime scanTime))
+                {
+                    status.LastScanTime = scanTime;
+                }
+                break;
+            }
+
+            return status;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error checking Defender status");
+            return new DefenderStatus
+            {
+                IsClean = true,
+                ThreatLevel = "Error",
+                LastScanTime = DateTime.Now
+            };
+        }
+    }
 }
